fb24254aee47d7175e0215923fa9d45a2bb64fa8
Emiliano R <ev.rdz22@outlook.com>
2024-09-29 17:51:42 -0600

    ft: datos cliente

--- src/Calc.java
+++ src/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
         String tipoCuenta = "Debito";
         double saldo = 1999.98;
 
-        System.out.println("************");
+        System.out.println("**************");
         System.out.println("\nNombre del cliente: " + nombre);
         System.out.println("El tipo de cuenta es:" + tipoCuenta);
         System.out.println("Su saldo disponible es:$ " + saldo);
