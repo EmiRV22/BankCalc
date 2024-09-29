479078c4264ff90c4a66aeaf8f69739499161ec9
Emiliano R <ev.rdz22@outlook.com>
2024-09-29 17:48:18 -0600

    new repo

---
+++ src/Calc.java
@@ -0,0 +1,13 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        String nombre = "Emiliano RV";
+        String tipoCuenta = "Debito";
+        double saldo = 1999.98;
+
+        System.out.println("************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es:" + tipoCuenta);
+        System.out.println("Su saldo disponible es:$ " + saldo);
+    }
+}
